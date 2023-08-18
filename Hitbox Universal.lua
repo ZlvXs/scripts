72eab0cdee780143f2c9339133a16c5c921e43d3
ZlvXs <141329634+ZlvXs@users.noreply.github.com>
2023-08-18 09:11:15 +0200

    Hitbox Universal

---
+++ Hitbox Universal.lua
@@ -0,0 +1,107 @@
+--Made By Zlv, Enjoy <3
+
+local CoreGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+
+getgenv().HitboxSize = 5
+getgenv().HitboxTransparency = 0.7
+
+getgenv().TeamCheck = false
+getgenv().FriendCheck = false
+
+--// UI
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/MyUILib(Unamed).lua"))();
+local Window = Library:Create("Hitbox Expander")
+
+local HomeTab = Window:Tab("Home","rbxassetid://12296135476")
+
+HomeTab:Section("Hitbox")
+
+HomeTab:Slider("Hitbox Size (Slider)", 0,300, function(value)
+    getgenv().HitboxSize = value
+end)
+
+HomeTab:TextBox("Hitbox Size (TextBox)", function(value)
+    getgenv().HitboxSize = value
+end)
+
+HomeTab:TextBox("Hitbox Transparency", function(number)
+    getgenv().HitboxTransparency = number
+end)
+
+HomeTab:Section("Setting")
+
+HomeTab:Toggle("Status: ", function(state)
+	getgenv().HitboxStatus = state
+    game:GetService('RunService').RenderStepped:connect(function()
+		if HitboxStatus == true and TeamCheck == false and FriendCheck == false then
+			for i,v in next, game:GetService('Players'):GetPlayers() do
+				if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+					pcall(function()
+						v.Character.HumanoidRootPart.Size = Vector3.new(HitboxSize, HitboxSize, HitboxSize)
+						v.Character.HumanoidRootPart.Transparency = HitboxTransparency
+						v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+						v.Character.HumanoidRootPart.Material = "Neon"
+						v.Character.HumanoidRootPart.CanCollide = false
+					end)
+				end
+			end
+        elseif HitboxStatus == true and TeamCheck == false and FriendCheck == true then
+            for i,v in next, game:GetService('Players'):GetPlayers() do
+                for i2,v2 in pairs(game:GetService('Players'):GetChildren()) do
+                    if v.Name ~= game:GetService('Players').LocalPlayer.Name and not v2:IsFriendsWith(game:GetService('Players').LocalPlayer.UserId) then
+                        pcall(function()
+                            v.Character.HumanoidRootPart.Size = Vector3.new(HitboxSize, HitboxSize, HitboxSize)
+                            v.Character.HumanoidRootPart.Transparency = HitboxTransparency
+                            v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+                            v.Character.HumanoidRootPart.Material = "Neon"
+                            v.Character.HumanoidRootPart.CanCollide = false
+                        end)
+                    end
+                end
+			end
+        elseif HitboxStatus == true and TeamCheck == true and FriendCheck == false then
+            for i,v in next, game:GetService('Players'):GetPlayers() do
+				if game:GetService('Players').LocalPlayer.Team ~= v.Team then
+					pcall(function()
+						v.Character.HumanoidRootPart.Size = Vector3.new(HitboxSize, HitboxSize, HitboxSize)
+						v.Character.HumanoidRootPart.Transparency = HitboxTransparency
+						v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+						v.Character.HumanoidRootPart.Material = "Neon"
+						v.Character.HumanoidRootPart.CanCollide = false
+					end)
+				end
+			end
+        elseif HitboxStatus == true and TeamCheck == true and FriendCheck == true then
+            for i,v in next, game:GetService('Players'):GetPlayers() do
+				if game:GetService('Players').LocalPlayer.Team ~= v.Team and not game:GetService('Players'):IsFriendsWith(UserId) then
+					pcall(function()
+						v.Character.HumanoidRootPart.Size = Vector3.new(HitboxSize, HitboxSize, HitboxSize)
+						v.Character.HumanoidRootPart.Transparency = HitboxTransparency
+						v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+						v.Character.HumanoidRootPart.Material = "Neon"
+						v.Character.HumanoidRootPart.CanCollide = false
+					end)
+				end
+			end
+		else
+		    for i,v in next, game:GetService('Players'):GetPlayers() do
+				if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+					pcall(function()
+						v.Character.HumanoidRootPart.Size = Vector3.new(2,2,1)
+						v.Character.HumanoidRootPart.Transparency = 1
+						v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Medium stone grey")
+						v.Character.HumanoidRootPart.Material = "Plastic"
+						v.Character.HumanoidRootPart.CanCollide = false
+					end)
+				end
+			end
+		end
+	end)
+end)
+
+
+HomeTab:Keybind("Toggle UI", Enum.KeyCode.F2, function()
+    Library:ToggleUI()
+end)
